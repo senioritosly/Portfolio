21d3e8ee2bd70977e8838c86376ab92a09d8a226
Cristales <sirsly073@gmail.com>
2023-05-25 16:19:52 -0600

    Router & Routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='App'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
