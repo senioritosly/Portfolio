4594f35a58982dcc84d0768d92fce04a3ece0139
Cristales <sirsly073@gmail.com>
2023-05-28 12:32:39 -0600

    Update Home.jsx

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import NavBar from "../components/NavBar/NavBar"
 
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <NavBar />
         </>
     )
 }
