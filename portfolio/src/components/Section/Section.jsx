dbaf16dff9cd81d87bc9629c8b9ff5bcb2fc4763
Cristales <sirsly073@gmail.com>
2023-05-29 18:36:07 -0600

    Name Section

---
+++ portfolio/src/components/Section/Section.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Section.module.css'
+import glob from './../../assets/images/eating_glob.svg'
+import glob_banner from './../../assets/images/glob_banner.svg'
+
+const Section = () => {
+    return (
+        <div className={styles.section_container}>
+            <div className={styles.main_section}>
+                <img src={glob} alt="globe" className={styles.glob} />
+                <div className={styles.main_text}>
+                    <h1 className={styles.title}>Hello.</h1>
+                    <h1 className={styles.title}>I'm Mario.</h1>
+                    <span className={styles.span}>I am majoring in comp sci, <br /> focused on front end development and design.</span>
+                </div>
+                <div className={styles.glob_banner}>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section
