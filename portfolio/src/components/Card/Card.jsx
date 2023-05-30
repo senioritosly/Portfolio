95fc1a3848e75983fad398ff403f7b7ecf4f5ffc
Cristales <sirsly073@gmail.com>
2023-05-30 15:49:35 -0600

    Card SB

--- portfolio/src/components/Card/Card.jsx
+++ portfolio/src/components/Card/Card.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './Card.module.css'
+import PropTypes from 'prop-types'
 
 const Card = () => {
   return (
     <div>Card</div>
   )
+}
+
+Card.propTypes = {
+    glob: PropTypes.string
 }
 
 export default Card
---
+++ portfolio/src/components/Card/Card.stories.js
@@ -0,0 +1,13 @@
+import Card from "./Card"
+
+export default {
+    title: 'Components/Card',
+    component: Card,
+    tags: ['autodocs'],
+}
+
+export const Thinking = {
+    args: {
+        glob: 'thinking'
+    }
+}
