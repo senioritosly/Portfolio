2573936d770ede30c84b929d66c95cf12c466f5f
Cristales <sirsly073@gmail.com>
2023-05-30 15:46:23 -0600

    Storybook

--- portfolio/.eslintrc.cjs
+++ portfolio/.eslintrc.cjs
@@ -1,32 +1,23 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb-base',
-        'airbnb/rules/react',
-        'plugin:prettier/recommended',
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-    ],
-    rules: {
-        "semi": [
-            "error",
-            "never"
-        ],
-        "max-len": [
-            "warn",
-            { "code": 120 }
-        ],
-        "prettier/prettier": ["warn", { "semi": false, "tabWidth": 4 }]
-    },
-}
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "airbnb-base", "airbnb/rules/react", "plugin:prettier/recommended", "plugin:storybook/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    "semi": ["error", "never"],
+    "max-len": ["warn", {
+      "code": 120
+    }],
+    "prettier/prettier": ["warn", {
+      "semi": false,
+      "tabWidth": 4
+    }]
+  }
+};
