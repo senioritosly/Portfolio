0adcfbef32f3eee13d58c9f877bde31b396104b3
Cristales <sirsly073@gmail.com>
2023-05-30 15:40:24 -0600

    Add Footer Projects

--- portfolio/src/pages/Projects/Projects.jsx
+++ portfolio/src/pages/Projects/Projects.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './Projects.module.css'
+
+// Imports for components
 import NavBar from '../../components/NavBar/NavBar'
+import Footer from '../../components/Footer/Footer'
+import Card from '../../components/Card/Card'
 
 const Projects = () => {
     return (
         <>
             <div className={styles.projects_container}>
                 <NavBar />
+                <div className={styles.projects_wrapper}>
+                    <footer className={styles.footer}>
+                        <Footer />
+                    </footer>
+                </div>
             </div>
         </>
     )
