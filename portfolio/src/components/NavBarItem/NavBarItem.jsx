2ad50e568e255ae08f6af5fe1c5e43e1ad932a1b
Cristales <sirsly073@gmail.com>
2023-05-31 17:43:16 -0600

    Update NavBarItem.jsx

--- portfolio/src/components/NavBarItem/NavBarItem.jsx
+++ portfolio/src/components/NavBarItem/NavBarItem.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import styles from './NavBarItem.module.css'
 
 const NavBarItem = ({ name, link, active }) => {
+  const handleClick = () => {
+    window.location.href = link
+  }
+
   return (
-    <div className={`${styles.navBarItems} ${active ? styles.navBarItems_active : ''}`}>
+    <div className={`${styles.navBarItems} ${active ? styles.navBarItems_active : ''}`} onClick={handleClick}>
       <a className={`${styles.item} ${active ? styles.item_active : ''}`} href={link}>{name}</a>
     </div>
   )
