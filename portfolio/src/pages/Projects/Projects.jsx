b903f8e2ee48f6fec4d94c39ffba1a185a2b69e4
Cristales <sirsly073@gmail.com>
2023-05-31 03:42:01 -0600

    Update Projects.jsx

--- portfolio/src/pages/Projects/Projects.jsx
+++ portfolio/src/pages/Projects/Projects.jsx
@@ -4,7 +4,6 @@ import styles from './Projects.module.css'
 // Imports for components
 import NavBar from '../../components/NavBar/NavBar'
 import Footer from '../../components/Footer/Footer'
-import Card from '../../components/Card/Card'
 
 const Projects = () => {
     return (
