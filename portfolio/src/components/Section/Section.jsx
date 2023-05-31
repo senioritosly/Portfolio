d61dcad06d61bb17687aa62ab8ecbbf1fd9117ff
Cristales <sirsly073@gmail.com>
2023-05-31 03:41:49 -0600

    Update Section.jsx

--- portfolio/src/components/Section/Section.jsx
+++ portfolio/src/components/Section/Section.jsx
@@ -10,7 +10,7 @@ const Section = () => {
                 <div className={styles.main_text}>
                     <h1 className={styles.title}>Hello.</h1>
                     <h1 className={styles.title}>I'm Mario.</h1>
-                    <span className={styles.span}>I am majoring in comp sci, <br /> focused on front end development and design.</span>
+                    <span className={styles.span}>I am majoring in computer science, <br /> focused on front-end development and design.</span>
                 </div>
                 <div className={styles.glob_banner}>
                 </div>
