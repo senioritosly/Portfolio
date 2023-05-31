53a269fca28fb531ab4d2df37337a6929b114301
Cristales <sirsly073@gmail.com>
2023-05-31 17:37:35 -0600

    Footer in Every Page

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './About.module.css'
+
+// Imports for components
 import NavBar from '../../components/NavBar/NavBar'
+import Footer from '../../components/Footer/Footer'
 
 const About = () => {
     return (
         <>
             <div className={styles.about_container}>
                 <NavBar />
+                <div className={styles.about_wrapper}>
+                    <footer className={styles.footer}>
+                        <Footer />
+                    </footer>
+                </div>
             </div>
         </>
     )
--- portfolio/src/pages/Contact/Contact.jsx
+++ portfolio/src/pages/Contact/Contact.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './Contact.module.css'
+
+// Imports for components
 import NavBar from '../../components/NavBar/NavBar'
+import Footer from '../../components/Footer/Footer'
 
 const Contact = () => {
     return (
         <>
             <div className={styles.contact_container}>
                 <NavBar />
+                <div className={styles.contact_wrapper}>
+                    <footer className={styles.footer}>
+                        <Footer />
+                    </footer>
+                </div>
             </div>
         </>
     )
