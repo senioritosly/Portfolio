0feedc91797f9b9e5c2c87a2b4a9841d7da02ae4
Cristales <sirsly073@gmail.com>
2023-06-01 00:26:24 -0600

    About Me Almost Done

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -4,6 +4,10 @@ import styles from './About.module.css'
 // Imports for components
 import NavBar from '../../components/NavBar/NavBar'
 import Footer from '../../components/Footer/Footer'
+import Card from '../../components/Card/Card'
+
+// Imports for images
+import portrait from '../../assets/images/Berserk_Sly.png'
 
 const About = () => {
     return (
@@ -11,6 +15,9 @@ const About = () => {
             <div className={styles.about_container}>
                 <NavBar />
                 <div className={styles.about_wrapper}>
+                    <div className={styles.about_card_wrapper}>
+                        <Card title={'About Me'} content={'My name is Mario Cristales, a rather odd surname, yeh?'} img={portrait} type={'me'}/>
+                    </div>
                     <footer className={styles.footer}>
                         <Footer />
                     </footer>
