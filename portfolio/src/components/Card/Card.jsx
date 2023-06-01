1a08419aeba8685346b0bcc0096893c0f07176c5
Cristales <sirsly073@gmail.com>
2023-06-01 00:26:16 -0600

    Card Component

--- portfolio/src/components/Card/Card.jsx
+++ portfolio/src/components/Card/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './Card.module.css'
 
-const Card = () => {
+// Imports for images
+
+const Card = ({ title, content, img, type }) => {
   return (
-    <div>Card</div>
+    <div className={styles.card_container}>
+      <div className={styles.card_content}>
+        <div className={styles.main_text}>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.description}>{content}</p>
+        </div>
+        <div className={`${styles[`portrait_container--${type}`]}`}>
+          <img src={img} alt="portrait" className={`${styles[`portrait--${type}`]}`} />
+        </div>
+      </div>
+    </div>
   )
 }
 
