a4e481468dbe07aded36d1c9ea5af49b5cf5cb48
Cristales <88349168+marcristales@users.noreply.github.com>
2023-06-02 12:19:53 -0600

    About Me Complete

--- portfolio/src/components/Card/Card.jsx
+++ portfolio/src/components/Card/Card.jsx
@@ -4,12 +4,19 @@ import styles from './Card.module.css'
 // Imports for images
 
 const Card = ({ title, content, img, type }) => {
+  const formattedContent = content.split('\n').map((line, index) => (
+    <React.Fragment key={index}>
+      {line}
+      <br />
+    </React.Fragment>
+  ))
+
   return (
     <div className={styles.card_container}>
       <div className={styles.card_content}>
         <div className={styles.main_text}>
           <h1 className={styles.title}>{title}</h1>
-          <p className={styles.description}>{content}</p>
+          <p className={styles.description}>{formattedContent}</p>
         </div>
         <div className={`${styles[`portrait_container--${type}`]}`}>
           <img src={img} alt="portrait" className={`${styles[`portrait--${type}`]}`} />
--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -10,13 +10,26 @@ import Card from '../../components/Card/Card'
 import portrait from '../../assets/images/Berserk_Sly.png'
 
 const About = () => {
+
+    const description = 
+    `My name is Mario Cristales, but many people online know me as Sir Sly.\n
+    I'm very passionate about programming, I've been focusing on all about programming since 2017.
+    I have been falling in love with design and web-development this year.
+    My main focus before was cyber-security, but I've realized I got something for design (as you could probably tell by my website).\n
+
+    I'm not a person of many hobbies, yet I do have some that I enjoy doing.
+    I love playing video games, I've been playing them since I was 4 years old thanks to my older brothers who introduced me to them.
+    I'm also very passionate about music, I've been wanting to make songs for a while now.
+    And last but not least, I am just getting into the whole dioramas and miniatures hobby, I've been wanting to get into it for a while now, but I never had the time to do so.\n
+    `
+
     return (
         <>
             <div className={styles.about_container}>
                 <NavBar />
                 <div className={styles.about_wrapper}>
                     <div className={styles.about_card_wrapper}>
-                        <Card title={'About Me'} content={'My name is Mario Cristales, a rather odd surname, yeh?'} img={portrait} type={'me'}/>
+                        <Card title={'About Me'} content={description} img={portrait} type={'me'}/>
                     </div>
                     <footer className={styles.footer}>
                         <Footer />
